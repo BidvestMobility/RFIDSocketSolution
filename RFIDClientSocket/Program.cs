ca314437aae0858c7e497ccd820f42186ba10e88
John <johnw@bidvestmobility.co.za>
2021-02-19 08:44:00 +0200

    Initial commit

---
+++ RFIDClientSocket/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient
+{
+
+    public static void StartClient()
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new byte[1024];
+
+        string instruction;
+
+        // Connect to a remote device.  
+        try
+        {
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+
+                Console.WriteLine("Type 'start' to start scanning. Type 'end' to end scanning.");
+
+                while (true)
+                {
+                    // Encode the data string into a byte array.  
+                    instruction = Console.ReadLine();
+                    //if (instruction.Equals("start") == true)
+                    //{
+                    byte[] msg = Encoding.ASCII.GetBytes(instruction);
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+                    //}
+                    // Clear msg.  
+                    //Array.Clear(msg, 0, msg.Length);
+
+                    // if (instruction.Equals("end") == true)
+                    //  {
+                    // Receive the response from the remote device. 
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine($"{Encoding.ASCII.GetString(bytes, 0, bytesRec)}");
+                    // }
+                }
+
+                // Release the socket.  
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"ArgumentNullException : {ex.Message}");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException : {0}", ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected exception : {0}", ex.ToString());
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        Console.ReadLine();
+    }
+
+    public static int Main(String[] args)
+    {
+        StartClient();
+        return 0;
+    }
+}
---
+++ RFIDServerSocket/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketListener
+{
+
+    // Incoming data from the client.  
+    public static string data = null;
+
+    public static void StartListening()
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the
+        // host running the application.  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for instruction from client...");
+
+            // Start listening for connections.  
+            while (true)
+            {
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();
+                data = null;
+
+            while (true)
+                {
+                    // An incoming connection needs to be processed.  
+                    int bytesRec = handler.Receive(bytes);
+                    data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    //if (data.IndexOf("<EOF>") > -1)
+                    //{
+                    //    break;
+                    //}
+                    //}
+
+                    if (data.Equals("start") == true)
+                    {
+                        Console.WriteLine("Scanning started...");
+                        // Echo the status back to the client.  
+                        byte[] msg = Encoding.ASCII.GetBytes("Server started scanning...");
+                        handler.Send(msg);
+                    }
+
+                    if (data.Equals("end") == true)
+                    {
+                        Console.WriteLine("Scanning ended...");
+                        // Echo the status back to the client.  
+                        byte[] msg = Encoding.ASCII.GetBytes("Server stopped scanning...");
+                        handler.Send(msg);
+                    }
+
+
+                    //Some code to store scanned data into array
+
+                }
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+    }
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
